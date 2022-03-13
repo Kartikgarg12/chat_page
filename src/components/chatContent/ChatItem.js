db8135541bcb7a043d76b84781a42acee292c46b
KARTIK GARG <33518558+Kartikgarg12@users.noreply.github.com>
2022-03-13 18:54:51 +0530

    Add files via upload

---
+++ src/components/chatContent/ChatItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class ChatItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div
+        className={`chat__item ${this.props.user ? this.props.user : ""}`}
+      >
+        <div className="chat__item__content">
+         <div className="chat__msg">{this.props.msg}</div>
+        </div>
+      </div>
+    );
+  }
+}
